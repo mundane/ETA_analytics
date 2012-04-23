079fb70c4db75eae6bd76aafeb5a1efa860f5dfd
Asger Pedersen <asger.pedersen@gmail.com>
2012-04-23 12:04:58 +0200

    Changed readme to markdown. Deleted old readme. Changes to GraphStore and MainView

---
+++ README.md
@@ -0,0 +1,5 @@
+# Project course Software development
+
+Android app, built using Sencha JavaScript library.
+
+App folder contains the actual code, other folders just contain prototypes and the like.
--- app/views/MainView.js
+++ app/views/MainView.js
@@ -7,15 +7,13 @@
         Ext.apply(AnalyticsApp.views, {
             notesListView: new AnalyticsApp.views.NotesListView({ analyticsStore: AnalyticsApp.stores.analyticsStore }),
             noteEditorView: new AnalyticsApp.views.NoteEditorView(),
-            analyticsImportView: new AnalyticsApp.views.AnalyticsImportView(),
-            defaultView1: new AnalyticsApp.views.DefaultView1()
+            analyticsImportView: new AnalyticsApp.views.AnalyticsImportView()
         });
 
         this.items = [
             AnalyticsApp.views.notesListView,
             AnalyticsApp.views.noteEditorView,
-            AnalyticsApp.views.analyticsImportView,
-			AnalyticsApp.views.defaultView1
+            AnalyticsApp.views.analyticsImportView
         ];
 
         AnalyticsApp.views.MainView.superclass.initComponent.call(this);
